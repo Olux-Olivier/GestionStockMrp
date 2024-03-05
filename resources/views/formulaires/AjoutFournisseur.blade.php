12d1620052c55f0ab6a6675f5f3175eebbd85922
Olivier Kasongo <olivierkasongo539@gmail.com>
2024-03-06 00:22:17 +0100

    view ajout fournisseur review

--- resources/views/formulaires/AjoutFournisseur.blade.php
+++ resources/views/formulaires/AjoutFournisseur.blade.php
@@ -9,21 +9,20 @@
 <body>
 
     <div class="container">
-    <h2>Ajout de Fournisseur</h2>
-    <form id="addSupplierForm">
-        <label for="supplierName">Nom du Fournisseur:</label>
-        <input type="text" id="supplierName" name="supplierName" required>
+        <h2>Ajout de Fournisseur</h2>
+        <form id="addSupplierForm" method="" action="">
+            <label for="supplierName">Nom du Fournisseur:</label>
+            <input type="text" id="supplierName" name="supplierName" required>
 
-        <label for="supplierEmail">Adresse E-mail:</label>
-        <input type="email" id="supplierEmail" name="supplierEmail" required>
+            <label for="supplierEmail">Adresse E-mail:</label>
+            <input type="email" id="supplierEmail" name="supplierEmail" required>
 
-        <label for="supplierAddress">Adresse Physique:</label>
-        <input type="text" id="supplierAddress" name="supplierAddress" required>
+            <label for="supplierAddress">Adresse Physique:</label>
+            <input type="text" id="supplierAddress" name="supplierAddress" required>
 
-        <input type="submit" value="Ajouter Fournisseur">
-    </form>
+            <input type="submit" value="Ajouter Fournisseur">
+        </form>
     </div>
-
 
 </body>
 </html>
